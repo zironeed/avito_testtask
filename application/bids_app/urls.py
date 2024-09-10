e13ab2925b15bf9bb8c5dde11083e7766253482f
Adil Salimgareev <forever@nzironeed.ru>
2024-09-10 23:34:53 +0500

    - added urls for applications and CRUD operations - added applications into INSTALLED APPS

---
+++ application/bids_app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .apps import BidsAppConfig
+
+
+app_name = BidsAppConfig.name
+
+
+urlpatterns = [
+    path('<int:tender_pk>/list/', BidListView.as_view(), name='bid_list'),
+    path('my/', BidListMyView.as_view(), name='bid_list_my'),
+    path('new/', BidCreateView.as_view(), name='bid_create'),
+    path('<int:pk>/edit/', BidUpdateView.as_view(), name='bid_update'),
+    path('<int:pk>/rollback/<int:version_id>/', BidRollbackView.as_view(), name='bid_rollback'),
+]
---
+++ application/tender_app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .apps import TenderAppConfig
+
+
+app_name = TenderAppConfig.name
+
+
+urlpatterns = [
+    path('', TenderListView.as_view(), name='tender_list'),
+    path('my/', TenderListMyView.as_view(), name='tender_list_my'),
+    path('new/', TenderCreateView.as_view(), name='tender_create'),
+    path('<int:pk>/edit/', TenderUpdateView.as_view(), name='tender_update'),
+    path('<int:pk>/rollback/<int:version_id>/', TenderRollbackView.as_view(), name='tender_rollback'),
+]
