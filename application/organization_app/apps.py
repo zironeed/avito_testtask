591d42bd4638dfbb58f2eaabad1bfff857782263
Adil Salimgareev <forever@nzironeed.ru>
2024-09-10 23:54:34 +0500

    - updated app.py files in all applications

--- application/organization_app/apps.py
+++ application/organization_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class OrganizationAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'organization_app'
+    name = 'application.organization_app'
--- application/tender_app/apps.py
+++ application/tender_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TenderAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tender_app'
+    name = 'application.tender_app'
