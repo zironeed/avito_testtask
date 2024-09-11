74c5287cafac5d214ed4c04fb743778f1ad7f42a
Adil Salimgareev <forever@nzironeed.ru>
2024-09-11 20:00:56 +0500

    added a new models organization_app: - Organization - OrganizationResponsible
    tender_app:
    - Tender
    - TenderVersion
    
    user_app:
    -Employee

--- application/tender_app/models.py
+++ application/tender_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Tender(models.Model):
+
+    class ServiceType(models.TextChoices):
+        SERVICE = 'Service'
+        PRODUCT = 'Product'
+        CONSTRUCTION = 'Construction'
+
+    name = models.CharField(max_length=100, verbose_name='Название тендера')
+    description = models.TextField(verbose_name='Описание тендера')
+    serviceType = models.CharField(choices=ServiceType.choices, max_length=30,
+                                   default=ServiceType.SERVICE, verbose_name='Тип услуги')
+
+    creator = models.ForeignKey('application.user_app.Employee', on_delete=models.CASCADE,
+                                related_name='tenders', verbose_name='Создатель тендера')
+    organization = models.ForeignKey('application.organization_app.Organization', on_delete=models.CASCADE,
+                                     related_name='tenders', verbose_name='Организация')
+
+    def __str__(self):
+        return self.name
+
+
+
+class TenderVersion(models.Model):
+    pass
--- application/user_app/models.py
+++ application/user_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+import uuid
 
-# Create your models here.
+class Employee(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
