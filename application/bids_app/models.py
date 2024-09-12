e815c4d038b1c4c6b3af4761620c3182bfd3ba5f
Adil Salimgareev <forever@nzironeed.ru>
2024-09-12 14:58:40 +0500

    added Bid model

--- application/bids_app/models.py
+++ application/bids_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Bid(models.Model):
+
+    class Status(models.TextChoices):
+        submitted = 'Submitted'
+        accepted = 'Accepted'
+        rejected = 'Rejected'
+
+    name = models.CharField(max_length=100, verbose_name='Название предложения')
+    description = models.TextField(verbose_name='Описание предложения')
+    status = models.CharField(choices=Status.choices, max_length=30, default=Status.submitted, verbose_name='Статус')
+
+    tenderId = models.ForeignKey('application.tender_app.Tender', on_delete=models.CASCADE, verbose_name='Тендер')
+    organizationId = models.ForeignKey('application.organization_app.Organization', on_delete=models.CASCADE,
+                                       verbose_name='Организация', null=True, blank=True)
+    creator = models.ForeignKey('application.user_app.Employee', on_delete=models.CASCADE, verbose_name='Создатель')
+
+    def __str__(self):
+        return self.name
