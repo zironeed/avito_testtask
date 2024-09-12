0e0abbb74f3c15d9f859b33850bdcdb21a8ae194
Adil Salimgareev <forever@nzironeed.ru>
2024-09-12 15:28:33 +0500

    added status and version fields for Tender model

--- application/tender_app/models.py
+++ application/tender_app/models.py
@@ -3,6 +3,11 @@ from django.db import models
 
 class Tender(models.Model):
 
+    class Status(models.TextChoices):
+        CREATED = 'CREATED'
+        PUBLISHED = 'PUBLISHED'
+        CLOSED = 'CLOSED'
+
     class ServiceType(models.TextChoices):
         SERVICE = 'Service'
         PRODUCT = 'Product'
@@ -12,6 +17,9 @@ class Tender(models.Model):
     description = models.TextField(verbose_name='Описание тендера')
     serviceType = models.CharField(choices=ServiceType.choices, max_length=30,
                                    default=ServiceType.SERVICE, verbose_name='Тип услуги')
+    status = models.CharField(choices=Status.choices, max_length=30,
+                              default=Status.CREATED, verbose_name='Статус тендера')
+    version = models.PositiveIntegerField(default=1, verbose_name='Версия')
 
     creator = models.ForeignKey('application.user_app.Employee', on_delete=models.CASCADE,
                                 related_name='tenders', verbose_name='Создатель тендера')
@@ -20,7 +28,6 @@ class Tender(models.Model):
 
     def __str__(self):
         return self.name
-
 
 
 class TenderVersion(models.Model):
