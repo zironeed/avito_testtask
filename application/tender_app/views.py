3e5a17052b835f5cc8843e3e3998560d085c115f
Adil Salimgareev <forever@nzironeed.ru>
2024-09-12 16:55:33 +0500

    updated views (list, mylist, update) for tender model

--- application/tender_app/views.py
+++ application/tender_app/views.py
@@ -1,11 +1,12 @@
 from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView
+from rest_framework.response import Response
 from application.custom_permissions import IsResponsible
 from .models import Tender
 from .serializers import TenderSerializer
 
 
 class TenderListView(ListAPIView):
-    queryset = Tender.objects.all()
+    queryset = Tender.objects.filter(status=Tender.Status.PUBLISHED)
     serializer_class = TenderSerializer
 
 
@@ -13,7 +14,8 @@ class TenderListMyView(ListAPIView):
     serializer_class = TenderSerializer
 
     def get_queryset(self):
-        return self.queryset.filter(creator=self.request.user)
+
+        return self.queryset.filter(organization=self.request.user.organization)
 
 
 class TenderCreateView(CreateAPIView):
@@ -26,6 +28,28 @@ class TenderUpdateView(UpdateAPIView):
     queryset = Tender.objects.all()
     serializer_class = TenderSerializer
     permission_classes = [IsResponsible]
+
+    def perform_action(self, tender, action):
+        if action == 'publish':
+            tender.status = Tender.Status.PUBLISHED
+        elif action == 'close':
+            tender.status = Tender.Status.CLOSED
+        tender.save()
+
+    def update(self, request, *args, **kwargs):
+        tender = self.get_object()
+        action = request.data.get('action')
+
+        if action in ['publish', 'close']:
+            self.perform_action(tender, action)
+        elif action == 'edit':
+            serializer = self.get_serializer(tender, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            tender.version += 1
+            serializer.save()
+
+        return Response(self.get_serializer(tender).data)
+
 
 class TenderRollbackView(UpdateAPIView):
     queryset = Tender.objects.all()
