f502864c9b283b8d798678fe14a933d328bef13c
Adil Salimgareev <forever@nzironeed.ru>
2024-09-12 19:30:02 +0500

    fixed ping function

--- application/views.py
+++ application/views.py
@@ -1,6 +1,8 @@
 from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-def ping():
-    return Response({'message': 'ok'}, status=status.HTTP_200_OK)
+@api_view(['GET'])
+def ping(request):
+    return Response({'message': 'ok (pong)'}, status=status.HTTP_200_OK)
