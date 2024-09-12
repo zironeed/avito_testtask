adfe532a2b6d7ee1e62691cfc9adbf6e01597c8d
Adil Salimgareev <forever@nzironeed.ru>
2024-09-12 22:46:18 +0500

    added filtering for Tender ListViews

--- application/tender_app/views.py
+++ application/tender_app/views.py
@@ -9,12 +9,20 @@ class TenderListView(ListAPIView):
     queryset = Tender.objects.filter(status=Tender.Status.PUBLISHED)
     serializer_class = TenderSerializer
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        service = self.request.query_params.get('serviceType')
+        if service:
+            return queryset.filter(serviceType__iexact=service)
+
 
 class TenderListMyView(ListAPIView):
     serializer_class = TenderSerializer
 
     def get_queryset(self):
-
+        service = self.request.query_params.get('serviceType')
+        if service:
+            return self.queryset.filter(organization=self.request.user.organization, serviceType__iexact=service)
         return self.queryset.filter(organization=self.request.user.organization)
 
 
