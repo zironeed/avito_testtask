f1be9adb99aa4e4ddbe0774cf36b4ea986ca8622
Adil Salimgareev <forever@nzironeed.ru>
2024-09-16 16:21:50 +0500

    feature: - updated requirements.txt - updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ set -e
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-gunicorn application.config.wsgi:application --bind 0.0.0.0:8080 --reload
+python manage.py runserver 0.0.0.0:8080
